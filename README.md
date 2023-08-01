6c78b3501f06e1a2e81d2a978c141fbc7f605722
Cristiano Carvalho <138523787+cristianomcarvalho@users.noreply.github.com>
2023-08-01 11:16:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# formulario-login-Dark
+ 
---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const mode = document.getElementById('mode_ico');
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+        return
+    }    
+    
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    form.classList.remove('dark');
+});
